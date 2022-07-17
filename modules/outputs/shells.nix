5f513a5c709b67df4c552e0b7fde23098d3b4614
Tristan Pemble <tpemble@gmail.com>
2022-07-16 17:51:45 -0700

    feat: development shells

--- modules/outputs/shells.nix
+++ modules/outputs/shells.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 with lib;
 
@@ -7,4 +7,26 @@ with lib;
     type = with types; attrsOf raw;
     default = {};
   };
+
+  options.outputs.devShells = mkOption {
+    type = with types; attrsOf (attrsOf package);
+    default = {};
+    internal = true;
+    visible = false;
+    apply = filterAttrs (n: v: (builtins.length (builtins.attrNames v) > 0));
+  };
+
+  config.outputs.devShells = genAttrs config.systems (system:
+    let
+      pkgs = import config.inputs.nixpkgs { inherit system; };
+    in mapAttrs (name: pkg:
+      let
+        flake = config.inputs.self;
+        callPackage = callPackageWith (pkgs // {
+          inherit name self flake;
+          inherit (flake) inputs outputs;
+        });
+        self = flake.outputs.devShells.${system}.${name};
+      in callPackage pkg {}
+    ) config.shells);
 }
